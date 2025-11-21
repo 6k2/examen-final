22de714f2592432b998a6c28cd953fe61c0cc7ec
6k2 (Juanes) <6k2.oficial@gmail.com>
2025-11-20 21:12:28 -0500

    Proyect and Firebase initialization

---
+++ src/firebaseConfig.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCNTPdVhKjIYEBTdsQ11B61RRsyfTDiFYg",
+  authDomain: "examen-final-3f37e.firebaseapp.com",
+  projectId: "examen-final-3f37e",
+  storageBucket: "examen-final-3f37e.firebasestorage.app",
+  messagingSenderId: "853388042882",
+  appId: "1:853388042882:web:5410ab7ce4faaa6e6dca9b",
+  measurementId: "G-5R2BPCLLN2"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+export { db };
+
